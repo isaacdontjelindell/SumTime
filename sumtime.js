6c57091140d455e3503856d6b224ca545925b2d8
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-27 13:15:02 -0600

    Fix #2 by using singleEvents: true to expand all recurring events

--- sumtime.js
+++ sumtime.js
@@ -82,7 +82,8 @@ function handleCalendarSelection (calendarId) {
     // get all events on the selected calendar
     gapi.client.load('calendar', 'v3', function () {
         var request = gapi.client.calendar.events.list({
-            'calendarId':calendarId
+            'calendarId':calendarId,
+            'singleEvents': true
         });
 
         request.execute(displayCalendarEventList);
@@ -101,26 +102,15 @@ function displayCalendarEventList (eventsResp) {
     $.each(events, function(index, event) {
         var eventName = event.summary;
 
-        if (!totals[eventName]) {
-            totals[eventName] = moment.duration(0);
-        }
-
         var start = moment(event.start.dateTime);
         var end = moment(event.end.dateTime);
         var duration = moment.duration(end.diff(start, 'minutes'), 'minutes');
         console.log('Event length: ' + duration.asMinutes()); // TODO remove
 
-        if (event.recurrence) {
-            var recurrence = event.recurrence[0].split(';'); // TODO can there ever be more than 1 recurrence rule??
-            var recurrenceCount = parseInt(recurrence[1].split('=')[1]);
-
-            for (var i=0; i<recurrenceCount-1; i++) {
-                duration = duration.add(duration);
-            }
+        if (!totals[eventName]) {
+            totals[eventName] = moment.duration(0);
         }
-
         totals[eventName].add(duration);
-        console.log('Total event length: ' + duration.asMinutes());
 
     });
 
